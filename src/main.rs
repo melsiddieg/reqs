25b033e6d677a0cd12a1fe3d077646a9fef14fd6
melsiddieg <melsiddieg@gmail.com>
2020-12-09 23:15:03 +0200

    writing files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use serde::{Deserialize};
+use serde::{Deserialize,Serialize};
 use reqwest::{Url,Error};
 use std::format;
 use std::time::Instant;
 use futures::future::try_join_all;
 use reqwest::Client;
+use std::fs::File;
+use serde_json;
+
 #[derive(Deserialize, Debug)]
 struct Top {
     results: Vec<Panels>,    
@@ -20,40 +23,47 @@ struct Panels {
 struct Stats {
     number_of_genes: u16,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize,Serialize, Debug)]
 struct Panel {
   id: u32,
-  hash_id: String,
+//   hash_id: String,
   name: String,
   disease_group: String,
   disease_sub_group: String,
   status: String,
   version: String,
-  relevant_disorder:Vec<String>,
+  relevant_disorders:Vec<String>,
   stats:StatInfo,
   genes:Vec<Gene>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize,Debug)]
 struct StatInfo{
     number_of_genes: u32,
-    // number_of_strs: u16,
-    // number_of_regions: u8,
+    number_of_strs: u16,
+    number_of_regions: u8,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize,Serialize,Debug)]
 struct Gene {
 gene_data:GeneInfo,
 entity_type:String,
 entity_name:String,
-confidence_level:u8,
-penetrance:String,
+confidence_level:String,
+mode_of_pathogenicity: Option<String>,
+penetrance:Option<String>,
+evidence:Option<Vec<String>>,
+publications:Option<Vec<String>>,
 mode_of_inheritance:String,
+phenotypes:Option<Vec<String>>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct GeneInfo{
-    alias:Vec<String>,
+    alias:Option<Vec<String>>,
+    biotype:Option<String>,
+    hgnc_id:Option<String>,
+    gene_name: Option<String>,
 }
 
 
@@ -70,10 +80,6 @@ async fn main() -> Result<(), Error> {
     }
 
     let results = try_join_all(gets).await?;
-    for chunk in &results {
-       
-            println!("num panels is {}",&chunk.results.len())        
-    }
     for chunk in &results{
         for panel in &chunk.results {
             if panel.version.as_str().starts_with("0"){
@@ -86,15 +92,21 @@ async fn main() -> Result<(), Error> {
         } 
     // second async loop  
     let mut gets2 = Vec::new();
-    for  id in &panels_ids[1..=20]{
+    for  id in &panels_ids{
         let get = get_panels(&client,*id);
         gets2.push(get);  
     }
-    let gene_panels = try_join_all(gets2).await?;
+    let panel_genes = try_join_all(gets2).await?;
+
+    for pan in &panel_genes {
+        println!("Now writing ... panel {} to file {}.json",pan.name,pan.id);
+        serde_json::to_writer(&File::create(format!("{}.json",pan.id)).unwrap(), &pan).unwrap();
+    }
     
     println!("\n Done! filtered panels number is {}",panels_ids.len());
     println!("\n Done! Elapsed time is: {} seconds", now.elapsed().as_secs_f64());
-    println!("last gene panel is {:?}", gene_panels[0]);
+    // println!("last gene panel is {:?}", panel_genes[0]);
+    
 
     Ok(())
 }
@@ -114,7 +126,7 @@ async fn get_panels(client: &Client, id:u32)-> Result<Panel, Error>{
         let response = client.get(url2).send().await?;
         let gene_panel:Panel = response.json().await?;         
 
-    Ok(())
+    Ok(gene_panel)
 }
 
 // working sequential version
@@ -207,3 +219,32 @@ async fn get_panels(client: &Client, id:u32)-> Result<Panel, Error>{
         //         println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
         //         panels_ids.push(panel.id);
         //     }
+        // opencga_panel = {
+        //     'id': str(panel_info['id']),
+        //     'name': panel_info['name'],
+        //     'categories': categories,
+        //     'phenotypes': phenotypes,
+        //     'tags': [],
+        //     'stats': {
+        //         'numberOfVariants': 0,
+        //         'numberOfGenes': len(genes),
+        //         'numberOfRegions': 0
+        //     },
+        //     'variants': [],
+        //     'genes': genes,
+        //     'regions': [],
+        //     'version': 1,
+        //     'source': {
+        //         'id': panel_info['id'],
+        //         'name': panel_info['name'],
+        //         'version': panel_info['version'],
+        //         'author': '',
+        //         'project': 'PanelApp (GEL)'
+        //     },
+        //     'creationDate': datetime.date.today().isoformat(),
+        //     'modificationDate': datetime.date.today().isoformat(),
+        //     'description': panel_info['disease_sub_group'] + ' (' + panel_info['disease_group'] + ')',
+        //     'attributes': {
+        //         'PanelAppInfo': panel
+        //     }
+        // }
