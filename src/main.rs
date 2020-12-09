38c425a45515fe78e31e648807644af16e6d1d52
melsiddieg@gmail.com <melsiddieg@gmail.com>
2020-12-09 17:11:55 +0200

    working without panels

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,155 @@ use std::format;
 use std::time::Instant;
 use futures::future::try_join_all;
 use reqwest::Client;
-// use std::vec;
+#[derive(Deserialize, Debug)]
+struct Top {
+    results: Vec<Panels>,    
+}
+
+#[derive(Deserialize, Debug)]
+struct Panels {
+    id:u32, 
+    stats: Stats,
+    name: String,
+    version: String,
+}
+#[derive(Deserialize, Debug)]
+struct Stats {
+    number_of_genes: u16,
+}
+#[derive(Deserialize, Debug)]
+struct Panel {
+  id: u32,
+  hash_id: String,
+  name: String,
+  disease_group: String,
+  disease_sub_group: String,
+  status: String,
+  version: String,
+  relevant_disorder:Vec<String>,
+  stats:StatInfo,
+  genes:Vec<Gene>,
+}
+
+#[derive(Deserialize, Debug)]
+struct StatInfo{
+    number_of_genes: u32,
+    // number_of_strs: u16,
+    // number_of_regions: u8,
+}
+
+#[derive(Deserialize, Debug)]
+struct Gene {
+gene_data:GeneInfo,
+entity_type:String,
+entity_name:String,
+confidence_level:u8,
+penetrance:String,
+mode_of_inheritance:String,
+}
+
+#[derive(Deserialize, Debug)]
+struct GeneInfo{
+    alias:Vec<String>,
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut panels_ids:Vec<u32> = Vec::new();
+    // let timeout = Duration::new(5, 0);
+    let client = Client::builder().build()?;
+    let mut gets = Vec::new();
+    let now = Instant::now();
+    for  page in vec![1,2,3]{
+        let get = get_res(&client,page);
+        gets.push(get);  
+    }
+
+    let results = try_join_all(gets).await?;
+    for chunk in &results {
+       
+            println!("num panels is {}",&chunk.results.len())        
+    }
+    for chunk in &results{
+        for panel in &chunk.results {
+            if panel.version.as_str().starts_with("0"){
+                // println!("Panel is not ready for interpretation: '{},{}",panel.name, panel.version);
+            }else {
+                // println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
+                panels_ids.push(panel.id);
+            }
+    }
+        } 
+    // second async loop  
+    let mut gets2 = Vec::new();
+    for  id in &panels_ids[1..=20]{
+        let get = get_panels(&client,*id);
+        gets2.push(get);  
+    }
+    let gene_panels = try_join_all(gets2).await?;
+    
+    println!("\n Done! filtered panels number is {}",panels_ids.len());
+    println!("\n Done! Elapsed time is: {} seconds", now.elapsed().as_secs_f64());
+    println!("last gene panel is {:?}", gene_panels[0]);
+
+    Ok(())
+}
+
+async fn get_res(client: &Client, page:u8)-> Result<Top, Error>{
+    let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/?page={}",page);
+        let url2 = Url::parse(&url).unwrap();    
+        let response = client.get(url2).send().await?;
+        let panels:Top = response.json().await?;         
+
+    Ok(panels)
+}
+
+async fn get_panels(client: &Client, id:u32)-> Result<Panel, Error>{
+    let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/{}",id);
+        let url2 = Url::parse(&url).unwrap();    
+        let response = client.get(url2).send().await?;
+        let gene_panel:Panel = response.json().await?;         
+
+    Ok(())
+}
+
+// working sequential version
+// for  page in vec![1,2,3]{
+//     let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/?page={}",page);
+//     let url2 = Url::parse(&url).unwrap();    
+//     let response = client.get(url2).send().await?;
+//     let panels:Top = response.json().await?;    
+//     for panel in panels.results.iter() {
+//         // println!("panel id is {} and the number of genes is {}", panel.id,panel.stats.number_of_genes);
+//         if panel.version.as_str().starts_with("0"){
+//             println!("Panel is not ready for interpretation: '{},{}",panel.name, panel.version);
+//         }else {
+//             println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
+//             panels_ids.push(panel.id);
+//         }
+        
+
+// }
+// }
+// println!("\n Done! filtered panels number is {}",panels_ids.len());
+// println!("Elapsed: {} seconds", now.elapsed().as_secs_f64());
+
+// model
+   // opencga_panel = {
+    //     'id': str(panel_info['id']),
+    //     'name': panel_info['name'],
+    //     'categories': categories,
+    //     'phenotypes': phenotypes,
+    //     'tags': [],
+    //     'stats': {
+    //         'numberOfVariants': 0,
+    //         'numberOfGenes': len(genes),
+    //         'numberOfRegions': 0
+    //     },
+    //     'variants': [],
+    //     'genes': genes}
+    // use std::vec;
 
 // #[derive(Deserialize, Debug)]
 // struct Res {
@@ -43,34 +191,9 @@ use reqwest::Client;
 //     println!("{:?}", top.response[0].result[0]);
 //     Ok(())
 // }
-#[derive(Deserialize, Debug)]
-struct Top {
-    results: Vec<Panels>,    
-}
-
-#[derive(Deserialize, Debug)]
-struct Panels {
-    id:u32, 
-    stats: Stats,
-    name: String,
-    version: String,
-}
-#[derive(Deserialize, Debug)]
-struct Stats {
-    number_of_genes: u16,
-}
 
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    // let mut panels_ids:Vec<u32> = Vec::new();
-    // let timeout = Duration::new(5, 0);
-    let client = Client::builder().build()?;
-    let mut gets = Vec::new();
-    let now = Instant::now();
-    for  page in vec![1,2,3]{
-        let get = get_res(&client,page);
-        gets.push(get);
+
     
         // let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/?page={}",page);
         // let url2 = Url::parse(&url).unwrap();    
@@ -84,75 +207,3 @@ async fn main() -> Result<(), Error> {
         //         println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
         //         panels_ids.push(panel.id);
         //     }
-            
-
-    }
-    let results = try_join_all(gets).await?;
-    // for chunk in Some(results) {
-    //     for panel in chunk.results.iter() {
-    //         if panel.version.as_str().starts_with("0"){
-    //             println!("Panel is not ready for interpretation: '{},{}",panel.name, panel.version);
-    //         }else {
-    //                 println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
-    //                 panels_ids.push(panel.id);
-    //          }
-
-    //     }
-            // println!("panel id is {} and the number of genes is {}", panel.id,panel.stats.number_of_genes);
-       
-
-    // println!("\n Done! filtered panels number is {}",panels_ids.len());
-
-    println!("num results = {}",results.len());
-    println!("result num 1 is {:?}",results[0]);
-    println!("\n Done! Elapsed time is: {} seconds", now.elapsed().as_secs_f64());
-
-
-    Ok(())
-}
-
-async fn get_res(client: &Client, page:u8)-> Result<Top, Error>{
-    let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/?page={}",page);
-        let url2 = Url::parse(&url).unwrap();    
-        let response = client.get(url2).send().await?;
-        let panels:Top = response.json().await?;         
-
-    Ok(panels)
-}
-
-
-// working sequential version
-// for  page in vec![1,2,3]{
-//     let url:String = format!("https://panelapp.genomicsengland.co.uk/api/v1/panels/?page={}",page);
-//     let url2 = Url::parse(&url).unwrap();    
-//     let response = client.get(url2).send().await?;
-//     let panels:Top = response.json().await?;    
-//     for panel in panels.results.iter() {
-//         // println!("panel id is {} and the number of genes is {}", panel.id,panel.stats.number_of_genes);
-//         if panel.version.as_str().starts_with("0"){
-//             println!("Panel is not ready for interpretation: '{},{}",panel.name, panel.version);
-//         }else {
-//             println!("Processing ... {} {} number of genes is {}",panel.name,panel.version,panel.stats.number_of_genes);
-//             panels_ids.push(panel.id);
-//         }
-        
-
-// }
-// }
-// println!("\n Done! filtered panels number is {}",panels_ids.len());
-// println!("Elapsed: {} seconds", now.elapsed().as_secs_f64());
-
-// model
-   // opencga_panel = {
-    //     'id': str(panel_info['id']),
-    //     'name': panel_info['name'],
-    //     'categories': categories,
-    //     'phenotypes': phenotypes,
-    //     'tags': [],
-    //     'stats': {
-    //         'numberOfVariants': 0,
-    //         'numberOfGenes': len(genes),
-    //         'numberOfRegions': 0
-    //     },
-    //     'variants': [],
-    //     'genes': genes}
